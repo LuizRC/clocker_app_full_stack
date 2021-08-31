a5665f821685bff433c4944ebdb5e4806e7a8777
LuizRC <rafael84treex@protonmail.com>
2021-08-31 12:22:21 -0300

    correção agenda

--- clocker/components/Agenda/index.js
+++ clocker/components/Agenda/index.js
@@ -1,8 +1,9 @@
 import { Button } from "@chakra-ui/button"
 import firebase from "./../../config/firebase"
+
 export const Agenda = () => {
-  
-  const logout = () => firebase.auth.signout()
+
+  const logout = () => firebase.auth().signOut()
     return (
       <div>
        <Button onClick={logout}>Sair</Button>
