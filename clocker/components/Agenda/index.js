472e5efb2c6fed1b42da0fda4d3f33c4944551be
LuizRC <rafael84treex@protonmail.com>
2021-08-31 11:52:40 -0300

    correção na signup

--- clocker/components/Agenda/index.js
+++ clocker/components/Agenda/index.js
@@ -1,7 +1,11 @@
+import { Button } from "@chakra-ui/button"
+import firebase from "./../../config/firebase"
 export const Agenda = () => {
+  
+  const logout = () => firebase.auth.signout()
     return (
       <div>
-       Agenda
+       <Button onClick={logout}>Sair</Button>
          
       </div>
     )
--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -1,7 +1,35 @@
+import { Container, Spinner } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Login, Agenda } from "../components";
 import firebase from './../config/firebase';
 
 export default function Home() {
+
+  const [auth, setAuth] = useState({
+    loading: true,
+    user: false
+
+  })
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setAuth({
+        loading: false,
+        user
+      })
+    })
+
+  }, [])
+
+  if (auth.loading) {
+    return (
+      <Container  p={4} centerContent>
+        <Spinner />
+      </Container>
+    )
+
+  }
 
   const authenticatedUser = firebase.auth().currentUser
 
